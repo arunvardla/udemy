68a6ad254669b34ea7069f52551ea62d2d3ed767
arunvaradala <arunvaradala@gmail.com>
2023-04-07 09:11:08 +0530

    assignments and projects

---
+++ projects/project1:GuessMyNumber/script.js
@@ -0,0 +1,62 @@
+const secretNumber = Math.trunc(Math.random() * 20)+1;
+let score = 20;
+let highscore = 0;
+//document.querySelector('.number').textContent = secretNumber;
+
+const displayMessage = function(message){
+    document.querySelector('.message').textContent = message;
+}
+
+document.querySelector(".check").addEventListener('click', function(){
+    const guess = Number(document.querySelector('.guess').value);
+    console.log(guess, typeof guess);
+    if(!guess){
+        displayMessage('⛔️No Number')
+    } else if(guess === secretNumber){
+        displayMessage('🎉 Correct Number')
+        document.querySelector("body").style.backgroundColor = '#60b347';
+        document.querySelector('.number').style.width = '30rem';
+        document.querySelector('.number').textContent = secretNumber;
+        if(score > highscore){
+            highscore = score;
+            document.querySelector('.highscore').textContent = highscore;
+        }
+    } else if(guess !== secretNumber){
+        if(score > 1){
+            displayMessage(guess > secretNumber ? "📈Too High" : "📈Too Low");
+            score--;
+            document.querySelector('.score').textContent = score;
+        } else{
+            displayMessage('💥 You lost the game!');
+        }
+    }
+    
+    // else if(guess > secretNumber){
+    //     if(score > 1){
+    //         displayMessage('📈Too High');
+    //         score--;
+    //         document.querySelector('.score').textContent = score;
+    //     } else {
+    //         displayMessage('💥 You lost the game!');
+    //     }
+    // } else if(guess < secretNumber){
+    //     if(score > 1){
+    //         displayMessage('📈Too Low');
+    //         score--;
+    //         document.querySelector('.score').textContent = score;
+    //     } else {
+    //         displayMessage('💥 You lost the game!');
+    //     }
+    // }
+});
+
+document.querySelector('.again').addEventListener('click', function () {
+    score = 20;
+    secretNumber = Math.trunc(Math.random()*20)+1;
+    displayMessage("Start guessing...")
+    document.querySelector('.score').textContent = score;
+    document.querySelector('.number').textContent = '?';
+    document.querySelector('.guess').textContent = '';
+    document.querySelector('body').style.backgroundColor = '#222';
+    document.querySelector('.number').style.width = '15rem';
+});
